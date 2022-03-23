20663fa88be05d3e13bf7d2b98e34bd6d57b09f5
aidanliddiard <aidanliddiard@gmail.com>
2022-03-23 16:33:48 -0700

    links working to display books information

--- src/views/Books/BookDetail.js
+++ src/views/Books/BookDetail.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
 import Book from '../../components/book/Book';
 import { getBookById } from '../../services/books';
+import { useParams } from 'react-router-dom';
 
 function BookDetail() {
-  const id = 1; // TODO: Use id from route
+  const params = useParams(); // TODO: Use id from route
+  console.log(params);
+
   const [book, setBook] = useState(null);
 
   useEffect(() => {
-    getBookById(id).then(({ data }) => setBook(data));
-  }, [id]);
+    getBookById(params.id).then(({ data }) => setBook(data));
+  }, [params.id]);
 
   if (!book) return <h3>Loading book...</h3>;
 
