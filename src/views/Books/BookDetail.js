8c72cca2d920910a11833ffefdd24cc6435380ad
aidanliddiard <aidanliddiard@gmail.com>
2022-03-23 16:42:14 -0700

    adding links between pages

--- src/views/Books/BookDetail.js
+++ src/views/Books/BookDetail.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Book from '../../components/book/Book';
 import { getBookById } from '../../services/books';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function BookDetail() {
   const params = useParams(); // TODO: Use id from route
@@ -15,7 +16,12 @@ function BookDetail() {
 
   if (!book) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Link to={`/books`}>Back to Catalog</Link>
+      <Book book={book} showDetail />
+    </>
+  );
 }
 
 export default BookDetail;
